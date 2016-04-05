db622af5ab1612e45e7acac67f86cc8ac1744a6e
munkefrugt@gmail.com <munkefrugt@gmail.com>
2016-04-05 12:18:19 +0200

    gui, nu med bevægelig firkant

--- src/com/company/Draw.java
+++ src/com/company/Draw.java
@@ -1,35 +1,138 @@
 package com.company;
 
+import javafx.scene.shape.Ellipse;
+
 import javax.swing.*;
 
 import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.awt.event.KeyEvent;
+import java.awt.event.KeyListener;
+import java.awt.geom.Arc2D;
+
 /**
  * Created by v on 4/4/16.
+ *
+ * inspired by : https://www.youtube.com/watch?v=p9Y-NBg8eto
  */
 // extends Jpanel, because we draw on the Jpanel, not on the frame.
-public class Draw extends JPanel
+public class Draw extends JPanel implements ActionListener,KeyListener
 {
+    Timer t = new Timer(5,this);
+    int x= 0, y=0 , velx = 0, vely = 0;
+
+    public Draw()
+    {
+        t.start();
+        addKeyListener(this);
+        // allows us to use the keyListener.
+        setFocusable(true);
+        // make other keys act normal. like the tab key.
+        setFocusTraversalKeysEnabled(false);
+    }
+
+
 
     public void drawing()
     {
         // repaint() calls the paintComponent(Graphics g) methode.
+
         repaint();
     }
 
+
+    // allows us to draw graphics on the screen
     public void paintComponent(Graphics g)
     {
         super.paintComponent(g);
-
+        //Graphics2D g2 = (Graphics2D) g;
+        //g2.fillOval(x, 40,y,  40);
+                //(new Ellipse2D.Double(x,y,40,40));
         g.setColor(Color.BLACK);
         // int x, int y, int width, int height.
-        g.fillRect(0,0,20,20);
-        g.fillRect(20,20,20,20);
+        g.fillRect(x,y,20,20);
+        /*g.fillRect(20,20,20,20);
         g.setColor(Color.ORANGE);
         g.fillRect(40,40,20,20);
         g.setColor(Color.CYAN);
-        g.fillOval(60,60,20,20);
+        g.fillOval(60,60,20,20);*/
+    }
 
-        
+    public void actionPerformed(ActionEvent e)
+    {
+        //System.out.println("repaint");
+        // paint again
+        repaint();
+
+        // change koordinates.
+        x+= velx;
+        y+= vely;
+
+    }
+
+    public void up()
+    {
+        vely= -1;
+        velx= 0;
+        System.out.println("up");
+    }
+
+    public void down()
+    {
+        vely= 1;
+        velx= 0;
+        System.out.println("down");
+    }
+
+    public void left()
+    {
+        vely= 0;
+        velx= -1;
+        System.out.println("left");
+    }
+
+    public void right()
+    {
+        vely= 0;
+        velx= 1;
+        System.out.println("right");
+    }
+
+
+        // inspired by : https://www.youtube.com/watch?v=p9Y-NBg8eto
+
+    @Override
+    public void keyPressed(KeyEvent e)
+    {
+
+        int code = e.getKeyCode();
+        if(code == KeyEvent.VK_UP)
+        {
+            up();
+        }
+        if(code == KeyEvent.VK_DOWN)
+        {
+            down();
+        }
+        if(code == KeyEvent.VK_LEFT)
+        {
+            left();
+        }
+        if(code == KeyEvent.VK_RIGHT)
+        {
+            right();
+        }
+    }
+    // the two methodes below needs to be included because we implemented Keylistener.
+    // but can stay empty, since we dont use them.
+    @Override
+    public void keyTyped(KeyEvent e)
+    {
+    }
+
+    @Override
+    public void keyReleased(KeyEvent e) {
 
     }
 }
--- src/com/company/Gui.java
+++ src/com/company/Gui.java
@@ -10,16 +10,17 @@ public class Gui extends JFrame {
     public Gui()
     {
         JFrame frame = new JFrame("BacktrackerLabyrinth");
-        //KeyboardExample keyboardExample = new KeyboardExample();
-        //frame.add(keyboardExample);
-        frame.setSize(400, 400);
-        frame.setVisible(true);
-        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         Draw draw = new Draw();
         // add the drawing to the frame.
         frame.add(draw);
 
-        draw.drawing();
+
+        //frame.add(keyboardExample);
+        frame.setVisible(true);
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
+        frame.setSize(800, 600);
+        //draw.drawing();
     }
 
 
